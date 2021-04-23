f173e28824a03e3d0f261e6e86ce2a9d15cf0c97
Niklas Jordan <hello@niklasjordan.com>
2021-04-23 15:02:19 +0200

    updating flake8 testing and settings.py SECRET_KEY

--- app/openspacedata/settings.py
+++ app/openspacedata/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
