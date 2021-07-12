1554a0b790be7e2ed56709dc90593e32634e018c
Niklas Jordan <hello@niklasjordan.com>
2021-07-12 14:55:11 +0200

    Add URL parameters, add geocoding for readable place names, limit the ouput

--- app/api/models.py
+++ app/api/models.py
@@ -8,7 +8,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_user(self, email, name, password=None):
         """Create a new user"""
-        
+
         if not email:
             raise ValueError('Users must have an email adress')
 
@@ -34,12 +34,12 @@ class UserProfileManager(BaseUserManager):
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database models for users in the system"""
-    
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
--- app/api/serializers.py
+++ app/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from . import views
 from api.models import Application, Indice, Satellite, Band
 from satsearch import Search
+import requests
 
 class IndiceSerializer(serializers.ModelSerializer):
 
@@ -26,52 +27,64 @@ class OsdSerializer(serializers.ModelSerializer):
     satellite = SatelliteSerializer(source='indice_to_use.satellite_to_use')
     indice = IndiceSerializer(source='indice_to_use')
     files = serializers.SerializerMethodField()
-    
+
     def get_files(self, instance):
-            bands = instance.bands
+        bands = instance.bands
 
-            # configuration
-            url = 'https://earth-search.aws.element84.com/v0' # URL to Sentinel 2 AWS catalog
-            collection = 'sentinel-s2-l2a-cogs'
+        # geocoding
+        place = self.context['location']
+        response = requests.get(f"https://nominatim.openstreetmap.org/search?q={place}&format=json&polygon_geojson=1&addressdetails=0&limit=1")
+        json_object = response.json()
+        bb = json_object[0]['boundingbox']
+        bbx = [float(x) for x in bb]
 
-            # search parameter
-            startDate = '2021-04-10'
-            endDate = '2021-04-12'
-            location = [ 13.6677,
-                    43.7232,
-                    16.2605,
-                    45.4522
-                ]
+        # aws configuration
+        url = 'https://earth-search.aws.element84.com/v0' # URL to Sentinel 2 AWS catalog
+        collection = 'sentinel-s2-l2a-cogs'
 
-            bbox_search = Search(
-                bbox=location, 
-                datetime=startDate+"/"+endDate, 
-                query={'eo:cloud_cover': {'lt': 50}},
-                collections=[collection],
-                url=url,
-                sort={'field': 'eo:cloud_cover', 'direction': 'desc'},
-            )
+        # aws search parameter
+        startDate = self.context['date_from']
+        endDate = self.context['date_to']
+        # location = [
+        #             11.40020,
+        #             53.63612,
+        #             11.44569,
+        #             53.62385
+        #             ]
 
-            items = bbox_search.items()
-            
-            downloads = {}
-            
-            for i, item in enumerate(items):
-                
-                data = {}
-                
-                data['Product ID']= item.properties["sentinel:product_id"]
-                data['Preview']= item.asset("thumbnail")["href"]
-                data['Date']= item.properties["datetime"]
-                data['Cloud cover']= item.properties["eo:cloud_cover"]
-                
-                for band in bands.split(','):
-                    data[band] = item.asset(band)["href"]
-                
-                downloads[i] = data
+        bbox_search = Search(
+            bbox=bbx,
+            datetime=startDate+"/"+endDate,
+            query={'eo:cloud_cover': {'lt': 50}},
+            collections=[collection],
+            url=url,
+            sort={'field': 'eo:cloud_cover', 'direction': 'desc'},
+        )
 
-            print(band)                        
-            return downloads
+        items = bbox_search.items()
+
+        downloads = {}
+
+        limit = 0
+
+        for i, item in enumerate(items):
+
+            data = {}
+
+            data['Product ID']= item.properties["sentinel:product_id"]
+            data['Preview']= item.asset("thumbnail")["href"]
+            data['Date']= item.properties["datetime"]
+            data['Cloud cover']= item.properties["eo:cloud_cover"]
+
+            for band in bands.split(','):
+                data[band] = item.asset(band)["href"]
+
+            downloads[i] = data
+
+            if i == limit:
+                break
+
+        return downloads
 
     class Meta:
         model = Application
